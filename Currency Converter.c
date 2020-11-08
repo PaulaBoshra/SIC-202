a63431bf1fd235d12408cbd25598dcc1bbd324d5
PaulaBoshra <64934133+PaulaBoshra@users.noreply.github.com>
2020-11-08 21:27:37 +0200

    Create Currency Converter.c

---
+++ Currency Converter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int x;
+    int e;
+    printf("Hello World!\n");
+    printf("Enter USD\n");
+    scanf("%d",&x);
+    e = x/16;
+    printf("EGP = %d \n",e);
+}
